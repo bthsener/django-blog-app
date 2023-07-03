6c6c7c4973f27b2df7f148194ec42581a4f99b4e
abatuhan06 <abatuhan0666@gmail.com>
2023-07-03 14:22:02 +0300

    added authenticated context

--- home/views.py
+++ home/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated:
+        context = {
+            'isim': 'batu'
+        }
+    else:
+        context = {
+            'isim': 'misafir'
+        }
+    return render(request, 'home.html', context)
