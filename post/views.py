00021e83d64cecbfa12c85523eca40e407dd9e0f
abatuhan06 <abatuhan0666@gmail.com>
2023-07-03 15:05:00 +0300

    added templates

--- post/views.py
+++ post/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from post.models import Post
 
-# Create your views here.
 def post_index(request):
-    return HttpResponse("<b>burası post index sayfası</b>")
+    posts = Post.objects.all()
+    return render(request, 'post/index.html', {'posts': posts})
 
 def post_details(request):
+    get_object_or_404(Post, id=1)
     return HttpResponse("<b>burası post details sayfası</b>")
 
 def post_create(request):
